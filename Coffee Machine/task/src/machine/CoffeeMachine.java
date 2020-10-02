0a3c3bcb6a147bca87f3daa3c973f2621b8fbb42
michalpom <michalpom@gmail.com>
2020-10-02 19:37:28 +0200

    Stage6 completed - working but not refactored
    The task was just to refactor and not teaching sth new
    so I just checked the solution from the previous stage and it worked

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -5,12 +5,9 @@ import java.util.Scanner;
 public class CoffeeMachine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-//        int waterSupply = 400, milkSupply = 540, coffeeBeansSupply = 120, money = 550;
-//        int cupsNeed = 0;
+
         MachineCoffee policeBuilding = new MachineCoffee();
 
-        //policeBuilding.coffeeMachineState();
-        //System.out.println("");
 
         while (true) {
 
@@ -36,11 +33,6 @@ public class CoffeeMachine {
 
         }
 
-//        //how many cups can make
-//        int cupsAvailable = howManyCupsCanMake(waterSupply, milkSupply, coffeeBeansSupply);
-//
-//        //result
-//        message(cupsNeed, cupsAvailable);
 
     }
 
